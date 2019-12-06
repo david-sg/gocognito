8b0a2d25b34c6f0a52621e5a9315d43a867a368b
David M <davidmosk@gmail.com>
2019-12-06 16:51:54 +0800

    initial commit

---
+++ extension/background.js
@@ -0,0 +1,32 @@
+/*
+ * Runs an action in background.
+ *
+ * Actions:
+ *   - CREATE_INCOGNITO_WINDOW: opens the current tab in a new incognito window
+ *
+ * Fired when the icon is clicked`.
+ */
+
+console.log('background started')
+
+
+
+chrome.browserAction.onClicked.addListener(buttonClicked)
+
+function buttonClicked(tab) {
+
+    let msg = {
+        txt: "incognito"
+    }
+    chrome.tabs.sendMessage(tab.id, msg)
+        // alert('working?');
+}
+
+chrome.runtime.onMessage.addListener(({ action, url }) => {
+    if (action === 'go_incognito') {
+        chrome.windows.create({
+            url,
+            incognito: true
+        })
+    }
+})
